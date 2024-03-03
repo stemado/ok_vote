89a5a2d1f3b60afe0f53cc2e60fdfdf2a3180502
stemado <istemado@gmail.com>
2024-03-02 21:39:59 -0600

    More changes. All sections from Word Docs can be identified by reading line-by-line and identifying a line with all capital letters.
    This will allow us to define methods for each section type (e.g. GENERAL ORDER, FIRST READING, SECOND READING, etc.)

--- file_writer.py
+++ file_writer.py
@@ -8,3 +8,29 @@ def write_to_file(filename, content):
     with open(filename, 'w') as f:
         f.write(content)
         f.write('\n')
+
+
+def split_text_by_all_caps_lines(text):
+    # Split text by lines
+    lines = text.split('\n')
+
+    # Initialize a list to hold chunks of text
+    chunks = []
+    # Initialize a temporary string to hold the current chunk of text
+    chunk = ""
+
+    for line in lines:
+        # If the line contains only capital letters (and possibly spaces)
+        if line.isupper():
+            # Add the current chunk to the chunks list
+            chunks.append(chunk.strip())
+            # Start new chunk with the all-caps line
+            chunk = line + "\n"
+        else:
+            # Add the line to the chunk
+            chunk += line + "\n"
+
+    # Don't forget to add the last chunk
+    chunks.append(chunk.strip())
+
+    return [c for c in chunks if c]
--- vote_parser.py
+++ vote_parser.py
@@ -1,61 +1,26 @@
 import re
 import pandas as pd
 from Classes.VoteDetail import VoteDetail, extract_section_by_keyword
-from file_writer import write_to_file
-from ok_legislature_config import ok_file_sections
+from file_writer import write_to_file, split_text_by_all_caps_lines
 from word_doc_helper import doc_to_docx, get_word_document_text
 
-def convert_to_dataframe(vote_details):
-    # Convert each VoteDetail object into a dictionary and add it to a list
-    data = [vars(vote_detail) for vote_detail in vote_details]
-
-    # Create a DataFrame from the list of dictionaries
-    df = pd.DataFrame(data)
-
-    return df
-
-def split_text_by_all_caps_lines(text):
-    # Split text by lines
-    lines = text.split('\n')
-
-    # Initialize a list to hold chunks of text
-    chunks = []
-    # Initialize a temporary string to hold the current chunk of text
-    chunk = ""
-
-    for line in lines:
-        # If the line contains only capital letters (and possibly spaces)
-        if line.isupper():
-            # Add the current chunk to the chunks list
-            chunks.append(chunk.strip())
-            # Start new chunk with the all-caps line
-            chunk = line + "\n"
-        else:
-            # Add the line to the chunk
-            chunk += line + "\n"
-
-    # Don't forget to add the last chunk
-    chunks.append(chunk.strip())
-
-    # Return the list of chunks, excluding any that are empty
-    return [c for c in chunks if c]
-
-# Option 2: Word Document
+# Word Document
 # Requires converting the doc to docx
 # assumes we are iterating through all the links we've already stored
-bill_url = '2023 HLeg Day61'
 # doc_file_path = r"C:\Users\sdoherty.ANTFARMLLC\Downloads\2023 HLeg Day61.doc"
 doc_file_path = r"/home/stephen/PycharmProjects/ok_vote/Journals/2023 HLeg Day16.docx"
 
 # docx_file = doc_to_docx(doc_file_path)
 text = get_word_document_text(doc_file_path)
 
-# Split our text - we can use all caps as our section delimitter
+# Split our text - we can use all caps as our section delimiter
+
 split_text = split_text_by_all_caps_lines(text)
 for t in split_text:
     print('------------NEW SECTION-------------------')
     print(t)
     print('------------END SECTION-------------------')
+    print(' ')
 
 for section in split_text:
 
@@ -63,7 +28,7 @@ for section in split_text:
     print('-------[NEW SECTION]: ' + section)
     print(result)
     print('-------[END SECTION]: ' + section)
-    print('')
+    print(' ')
 
 
 
