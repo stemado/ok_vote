e8a2fca87c9a85880a2e1f0a2f501ccdc50746ae
stemado <istemado@gmail.com>
2024-03-05 23:55:49 -0600

    Updates. Db if you know how to use. But also outputs as csv.
    Just click Debug button at top of your computer looks like a little green bug.
    
    The final output file will be stored in your downloads folder.

---
+++ Configuration/OkLegislativeConfig.py
@@ -0,0 +1,4 @@
+
+# Urls we'll re-use
+base_house_support_docs_url = 'http://webserver1.lsb.state.ok.us/cf/2021-22%20SUPPORT%20DOCUMENTS/votes/House/'
+base_house_doc_url = 'http://webserver1.lsb.state.ok.us/cf/2023-24%20JOURNAL/House/'
